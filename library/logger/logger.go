81605536b7c1e88038e0b2c4d0f425e649f9661e
zhanbai <bai.zhan@qq.com>
2020-09-14 15:06:18 +0800

    feat:logger

---
+++ library/logger/logger.go
@@ -0,0 +1,27 @@
+package logger
+
+import (
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+var (
+	Trace   *log.Logger
+	Info    *log.Logger
+	Warning *log.Logger
+	Error   *log.Logger
+)
+
+func init() {
+	errorFile, err := os.OpenFile("log/errors.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	Trace = log.New(ioutil.Discard, "TRACE: ", log.Ldate|log.Ltime|log.Lshortfile)
+	Info = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	Warning = log.New(os.Stdout, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	Error = log.New(io.MultiWriter(errorFile, os.Stderr), "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+}
